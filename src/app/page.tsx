345c683a463f87c269421a5a531e3286a06af429
sachin <sachinnarofficials@gmail.com>
2025-09-28 20:46:16 +0530

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Hero from "./components/UI/Hero";
 import Counter from "./components/UI/Counter";
-import Services from "./components/UI/Services";
 import About from "./components/UI/About";
 import Team from "./components/UI/Team";
 import Blog from "./components/UI/Blog";
