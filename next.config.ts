75387b17679ecb14052d4116f492fe5be49eeae6
sachin <sachinnarofficials@gmail.com>
2025-09-28 21:08:51 +0530

    deploment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  distDir: "build", // Custom build directory name
+  // distDir: "build", // Custom build directory name
 };
 
 export default nextConfig;
